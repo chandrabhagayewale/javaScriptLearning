76f97e9bfaa348b77469c1e9055bef13c68c4cb2
chandrabhagayewale <pratibhayewale22@gmail.com>
2022-12-09 23:46:14 +0530

    swapping variable
    swapping variable

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,22 @@
+console.log("================================using two variable===============");
+console.log("***Before Swapping***");
+var nameFirst="Sweety";
+var nameSecond="Cutie" ;
+console.log(nameFirst,nameSecond);
+ var temp=nameFirst ; 
+ console.log("***after Swapping***");
+nameFirst=nameSecond
+nameSecond=temp
+console.log(nameFirst,nameSecond);
+console.log("========================using three variable===============");
+console.log("***Before Swapping***");
+var num1="100";
+var num2="200";
+var num3="300";
+console.log(num1,num2,num3);
+var temp=num1;
+console.log("***after Swapping***");
+num1=num2
+num2=num3
+num3=temp
+console.log(num1,num2,num3);
