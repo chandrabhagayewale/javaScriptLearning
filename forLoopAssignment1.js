0c44f77e9ad682bcbb327fff30427a8a332af59f
chandrabhagayewale <pratibhayewale22@gmail.com>
2022-12-20 22:52:24 +0530

    forLoopAssignment1,forLoopAssignment2
    forLoopAssignment1,forLoopAssignment2

---
+++ forLoopAssignment1.js
@@ -0,0 +1,27 @@
+console.log(`-----------------numbers from 5 to 15 increment by 1-----------------------`)
+for (let index = 5; index<=15; index++) {
+    console.log(index);
+    
+}
+console.log(`-----------------numbers from 50 to 40 decrement by 1-----------------------`)
+for (let index=50; index>=40; index--) {
+    console.log(index);}
+    console.log(`-----------------Table of 5 no.-----------------------`)
+for (let index=5; index<=50; index=index+5) {
+    console.log(index);}
+    console.log(`-----------------Table of 10 no.-----------------------`)
+for (let index=10; index<=100; index=index+10) {
+    console.log(index);}
+    console.log(`-----------------Table of 10 no. reversed -----------------------`)
+for (let index=100; index>=10; index=index-10) {
+        console.log(index);}
+
+        console.log(`-----------------Table of 10 no. reversed -----------------------`)
+for (let index=1; index>=15; index=index+2) {
+            console.log(index);}
+    
+
+
+
+    
+
